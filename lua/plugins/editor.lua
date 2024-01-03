9d5e17f9aac52f5bcc93967f9f4a8783f62c405e
Maxime Cappellen <maxime.cappellen.e@thalesdigital.io>
2024-01-03 08:38:59 +0100

    feat(personal-config): add gitsigns and Mini.move

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -30,6 +30,27 @@ return {
     version = false,
     config = function()
       require("mini.surround").setup()
+      require("mini.move").setup({
+        mappings = {
+          -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+          left = "<S-h>",
+          right = "<S-l>",
+          up = "<S-k>",
+          down = "<S-j>",
+
+          -- Move current line in Normal mode
+          line_left = "<S-h>",
+          line_right = "<S-l>",
+          line_up = "<S-k>",
+          line_down = "<S-j>",
+        },
+
+        -- Options which control moving behavior
+        options = {
+          -- Automatically reindent selection during linewise vertical move
+          reindent_linewise = true,
+        },
+      })
     end,
   },
   {
