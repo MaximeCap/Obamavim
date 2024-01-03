6ca916fcefa0915d4f2ecc11b71068700b1d19e7
Maxime Cappellen <maxime.cappellen.e@thalesdigital.io>
2024-01-03 18:52:47 +0100

    wip(personal-config): current work

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -15,22 +15,22 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map("n", "<C-,>", "<Cmd>BufferPrevious<CR>", opts)
-map("n", "<C-.>", "<Cmd>BufferNext<CR>", opts)
+map("n", "<leader>,", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "<leader>.", "<Cmd>BufferNext<CR>", opts)
 -- Re-order to previous/next
-map("n", "<C-<>", "<Cmd>BufferMovePrevious<CR>", opts)
-map("n", "<C->>", "<Cmd>BufferMoveNext<CR>", opts)
+map("n", "<leader><", "<Cmd>BufferMovePrevious<CR>", opts)
+map("n", "<leader>>", "<Cmd>BufferMoveNext<CR>", opts)
 -- Goto buffer in position...
-map("n", "<C-1>", "<Cmd>BufferGoto 1<CR>", opts)
-map("n", "<C-2>", "<Cmd>BufferGoto 2<CR>", opts)
-map("n", "<C-3>", "<Cmd>BufferGoto 3<CR>", opts)
-map("n", "<C-4>", "<Cmd>BufferGoto 4<CR>", opts)
-map("n", "<C-5>", "<Cmd>BufferGoto 5<CR>", opts)
-map("n", "<C-6>", "<Cmd>BufferGoto 6<CR>", opts)
-map("n", "<C-7>", "<Cmd>BufferGoto 7<CR>", opts)
-map("n", "<C-8>", "<Cmd>BufferGoto 8<CR>", opts)
-map("n", "<C-9>", "<Cmd>BufferGoto 9<CR>", opts)
-map("n", "<C-0>", "<Cmd>BufferLast<CR>", opts)
+map("n", "<leader>t1", "<Cmd>BufferGoto 1<CR>", opts)
+map("n", "<leader>t2", "<Cmd>BufferGoto 2<CR>", opts)
+map("n", "<leader>t3", "<Cmd>BufferGoto 3<CR>", opts)
+map("n", "<leader>t4", "<Cmd>BufferGoto 4<CR>", opts)
+map("n", "<leader>t5", "<Cmd>BufferGoto 5<CR>", opts)
+map("n", "<leader>t6", "<Cmd>BufferGoto 6<CR>", opts)
+map("n", "<leader>t7", "<Cmd>BufferGoto 7<CR>", opts)
+map("n", "<leader>t8", "<Cmd>BufferGoto 8<CR>", opts)
+map("n", "<leader>t9", "<Cmd>BufferGoto 9<CR>", opts)
+map("n", "<leader>t0", "<Cmd>BufferLast<CR>", opts)
 -- Pin/unpin buffer
 map("n", "<C-p>", "<Cmd>BufferPin<CR>", opts)
 -- Close buffer
